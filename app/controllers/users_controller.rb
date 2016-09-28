783c4b8384fa7babcb790c18449512cbd84553b9
Jacob Vander Schel <jacob-vanderschel@uiowa.edu>
2016-09-28 03:43:12 +0000

    users/new works, fixing create now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
     end
     
     def create
-        if User.find?(user_params.user_id) == false
-            @user = User.create_user!(user_params)
-            flash[:notice] = "Welcome #(@user.user_id}. Your account has been created."
+        if User.find?(params[:user_id]) == false
+            @user = User.create_user!(params[:user])
+            flash[:notice] = "Welcome #{@user.user_id}. Your account has been created."
             redirect_to movies_path
         else
            flash[:warning] = "Sorry, this user-id is taken. Try again."
