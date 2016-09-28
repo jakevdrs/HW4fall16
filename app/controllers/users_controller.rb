dad1f720d2c58d4c6651e48e172e6be66506b94a
Jacob Vander Schel <jacob-vanderschel@uiowa.edu>
2016-09-28 03:55:18 +0000

    users/new works, fixing create now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
     end
     
     def create
-        if User.find(params[:user_id]) == nil
-            @user = User.create_user!(params[:user])
+        @user = User.create_user!(params[:user])
+        
+        if !!(@user)
             flash[:notice] = "Welcome #{@user.user_id}. Your account has been created."
             redirect_to movies_path
         else
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
     def create_user!(user_hash)
         session[:session_token] = SecureRandom.base64
         user_hash << session[:session_token]
-        @user = User.create!(user_hash)
+        return User.create!(user_hash)
     end
 end
