6d273c227de9632ff5b5a1aaea7d8626ff4b89af
Jacob Vander Schel <jacob-vanderschel@uiowa.edu>
2016-09-30 01:23:21 +0000

    working on users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,17 @@ class UsersController < ApplicationController
         params.require(:user_id).permit(:email, :session_token)
     end
     
+    def show
+        id = params[:id]
+        @user = User.find(id)
+    end
+    
     def new
        # default: render 'new' template
+    end
+    
+    def index
+        @users = User.all
     end
     
     def create
@@ -18,5 +27,23 @@ class UsersController < ApplicationController
            flash[:warning] = "Sorry, this user-id is taken. Try again."
            redirect_to new_user_path
         end
+    end
+    
+    def edit
+        @user = User.find params[:id]
+    end
+    
+    def update
+        @user = User.find params[:id]
+        @user.update_attributes!(user_params)
+        flash[:notice] = "#{@user.title} was successfully updated."
+        redirect_to user_path(@user)
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:notice] = "User '#{@user.title}' deleted."
+        redirect_to users_path
     end
 end
